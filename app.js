7568cc3aba5e77520dadc467a53b43ce28bec580
yundingy <yunding@vip.qq.com>
2020-01-16 17:13:59 +0800

    readme

--- app.js
+++ app.js
@@ -1,11 +1,92 @@
-var app = new Framework7({
+const app = new Framework7({
     root: '#app',
     name: 'My App',
     id: 'com.myapp.test',
     panel: {
         swipe: 'left',
     },
-    routes: routes
+    dialog: {
+        buttonOk: '确定',
+        buttonCancel: '取消'
+    },
+    routes: routes,
+    methods: {
+      toast(message, status) {
+          if (status === true) {
+              app.toast.create({
+                  icon: '<i class="fa fa-5x fa-check"></i>',
+                  text: message,
+                  position: 'center',
+                  closeTimeout: 2000,
+              }).open();
+          }else if (status === false) {
+              app.toast.create({
+                  icon: '<i class="fa fa-5x fa-close"></i>',
+                  text: message,
+                  position: 'center',
+                  closeTimeout: 2000,
+              }).open();
+          }else{
+              app.toast.create({
+                  text: message,
+                  position: 'center',
+                  closeTimeout: 2000,
+              }).open();
+          }
+      },
+      alert(status, message) {
+
+      }
+    },
+    on: {
+        init: function(e) {
+            this.views.create('.view-main');
+            if (!localStorage.getItem('role')) {
+                this.views.main.router.navigate('/login');
+            }else{
+                this.views.main.router.navigate('/index');
+            }
+        },
+        pageInit: function (page) {
+
+        }
+    }
 });
 
-var mainView = app.views.create('.view-main');
+const ajax = axios.create({
+    baseURL: 'http://47.240.38.193:32616/api',
+    //baseURL: 'http://127.0.0.1:32616/api',
+    timeout: 10000,
+    headers: {'role': localStorage.getItem('role')}
+});
+
+ajax.interceptors.response.use(function (response) {
+    if (response.status !== 200) {
+        app.toast.create({
+            text: '请求失败',
+            position: 'center',
+            closeTimeout: 2000,
+        }).open();
+        return Promise.reject('请求失败');
+    }
+    return response.data;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [ o[this.name] ];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,34 @@
 const routes = [
     {
-        path: '/about/',
+        path: '/login',
+        componentUrl: 'pages/login.html',
+    },
+    {
+        path: '/index',
+        componentUrl: 'pages/index.html',
+    },
+    {
+        path: '/about',
         componentUrl: 'pages/about.html',
     },
     {
-        path: '/detail/',
+        path: '/detail',
         componentUrl: 'pages/detail.html',
     },
+    {
+        path: '/apply',
+        componentUrl: 'pages/apply.html',
+    },
+    {
+        path: '/approve',
+        componentUrl: 'pages/approve.html',
+    },
+    {
+        path: '/approveDetail',
+        componentUrl: 'pages/approveDetail.html',
+    },
+    {
+        path: '/income',
+        componentUrl: 'pages/income.html',
+    }
 ]
